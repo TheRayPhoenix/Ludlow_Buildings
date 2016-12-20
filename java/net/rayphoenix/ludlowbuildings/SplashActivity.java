46fb4249880ed42f0339da4d39790eed6fb6dab3
Ray Phoenix <ray@rayphoenix.net>
2016-12-20 16:54:37 -0500

    Beginning

--- java/net/rayphoenix/ludlowbuildings/SplashActivity.java
+++ java/net/rayphoenix/ludlowbuildings/SplashActivity.java
@@ -1,7 +1,11 @@
 package net.rayphoenix.ludlowbuildings;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Create a TimerTask to wait ten seconds and then...
+        TimerTask task = new TimerTask(){
+
+            @Override
+            public void run() {
+                finish();
+                //... start MainActivity
+                startActivity(new Intent(SplashActivity.this, ItemListActivity.class));
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task,10000); // <- 10 seconds
     }
 }
