3a69ca68160a2798a94ac29e13ae11922e436a1c
Ray Phoenix <ray@rayphoenix.net>
2016-12-21 00:50:42 -0500

    SplashTrack Added

--- java/net/rayphoenix/ludlowbuildings/SplashActivity.java
+++ java/net/rayphoenix/ludlowbuildings/SplashActivity.java
@@ -1,6 +1,7 @@
 package net.rayphoenix.ludlowbuildings;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,6 +14,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Generate and start MediaPlayer with ten second clip of "Closer To The Heart" opening
+        MediaPlayer mpSplash;
+        mpSplash = MediaPlayer.create(this, R.raw.splashtrack);
+        mpSplash.start();
 
         //Create a TimerTask to wait ten seconds and then...
         TimerTask task = new TimerTask(){
