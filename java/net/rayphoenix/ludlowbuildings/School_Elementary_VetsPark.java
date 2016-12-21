5e02704e857b139a665e07dda3025d9653d84aab
Ray Phoenix <ray@rayphoenix.net>
2016-12-20 20:48:07 -0500

    Activities and strings added

---
+++ java/net/rayphoenix/ludlowbuildings/School_Elementary_VetsPark.java
@@ -0,0 +1,13 @@
+package net.rayphoenix.ludlowbuildings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class School_Elementary_VetsPark extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school__elementary__vets_park);
+    }
+}
